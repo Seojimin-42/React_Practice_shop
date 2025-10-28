bef3710fc0581122d42684d7292a603c1f5f7ee3
서지민 <sjm1172@gmail.com>
2025-10-28 22:31:00 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Button, Navbar, Container, Nav } from 'react-bootstrap'; // 컴포넌트 import 해야 사용가능
+import { useState } from "react";
 import bg from './img/bg.png'
 import plant from './img/plant.png'
+import data from './data.js';
 
 function App() {
+
+  let [plants] = useState(data)
+
   return (
     <div className="App">
        <Navbar bg="dark" data-bs-theme="dark">
@@ -25,18 +30,18 @@ function App() {
           <div className="row">
             <div className="col-md-4">
               <img src={plant} width="80%"></img>
-              <h4>상품명</h4>
-              <p>상품설명</p>
+              <h4>{plants[0].title}</h4>
+              <p>{plants[0].price}</p>
             </div>
             <div className="col-md-4">
               <img src={plant} width="80%"></img>
-              <h4>상품명</h4>
-              <p>상품설명</p>
+              <h4>{plants[1].title}</h4>
+              <p>{plants[1].price}</p>
             </div>
             <div className="col-md-4">
               <img src={plant} width="80%"></img>
-              <h4>상품명</h4>
-              <p>상품설명</p>
+              <h4>{plants[2].title}</h4>
+              <p>{plants[2].price}</p>
             </div>
           </div>
         </div> 
