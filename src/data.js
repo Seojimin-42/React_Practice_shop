3eff9465a9385ab0edb6162ea1af189a94c3505b
서지민 <sjm1172@gmail.com>
2025-10-30 20:53:35 +0900

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,24 +1,25 @@
 let data = [
   {
     id : 0,
-    title : "White and Black",
+    title : "라벤더",
     content : "Born in France",
     price : 120000
   },
 
   {
     id : 1,
-    title : "Red Knit",
+    title : "장미",
     content : "Born in Seoul",
     price : 110000
   },
 
   {
     id : 2,
-    title : "Grey Yordan",
-    content : "Born in the States",
+    title : "동백꽃",
+    content : "Born in Seoul",
     price : 130000
   }
 ] 
+
 
 export default data;
